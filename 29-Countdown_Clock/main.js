c390486a832779e26c8c3314c4f7411cba050a69
Jared <jaredarodriguez@gmail.com>
2020-07-07 08:04:24 -0400

    displayEndTime

--- 29-Countdown_Clock/main.js
+++ 29-Countdown_Clock/main.js
@@ -1,10 +1,13 @@
 let countdown; 
-const timerDisplay = document.querySelector('.display__time-left')
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
 function timer(seconds) {
     const now = Date.now(); 
     const then = now + seconds * 1000; 
     displayTimeLeft(seconds)
-    
+    displayEndTime(then); 
+
 countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000); 
         if (secondsLeft < 0) {
@@ -21,4 +24,11 @@ function displayTimeLeft(seconds){
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
     document.title = display; 
     timerDisplay.textContent = display; 
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp); 
+    const hour = end.getHours(); 
+    const minutes = end.getMinutes(); 
+    endTime.textContent = `Be Back at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`; 
 }
